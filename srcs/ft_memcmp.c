66fe2ccb0cd5adfdea3bb45ef5b1b1e84b9393fc
Axel Viala <aviala@student.42.fr>
2013-11-21 17:02:23 +0100

    ft_memcmp.c

---
+++ srcs/ft_memcmp.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	const unsigned 	*ps1, 
+					*ps2;
+	
+	ps1 = s1;
+	ps2 = s2;
+	i = 0;
+	while (i < n)
+	{
+		if(ps1[i] != ps2[i])
+		{
+			return (ps1[i] - ps2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
