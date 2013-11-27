9f25305df371825c0ad0108e13c1f07328d2e560
Axel Viala <aviala@student.42.fr>
2013-11-27 18:27:25 +0100

    changing place of the return in case of s2[i] == c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: aviala <aviala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 13:52:40 by aviala            #+#    #+#             */
-/*   Updated: 2013/11/26 18:16:35 by aviala           ###   ########.fr       */
+/*   Updated: 2013/11/27 18:26:35 by aviala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,20 @@
 
 void *ft_memccpy(void *s1, const void *s2, int c, size_t n)
 {
-	size_t		i;
-	char 		*cpy_s1;
-	const char	*cpy_s2;
+	size_t			i;
+	char			*cpy_s1;
+	const char		*cpy_s2;
+	unsigned char	c_cpy;
 
 	cpy_s1 = s1;
 	cpy_s2 = s2;
+	c_cpy = (unsigned char) c;
 	i = 0;
 	while (i < n)
 	{
-		if (cpy_s1[i] != (unsigned char)c)
-			return (&cpy_s1[i + 1]);
-
 		cpy_s1[i] = cpy_s2[i];
+		if (cpy_s2[i] == c_cpy)
+			return (&cpy_s1[i + 1]);
 		i++;
 	}
 	return (NULL);
