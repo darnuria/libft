1afde71b4d67ec091d7ede6bc7db49f86429a4b4
Axel Viala <aviala@student.42.fr>
2013-11-27 10:36:22 +0100

    Fixed ft_isascii to include ascii:127(decimal) character

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,10 +6,9 @@
 /*   By: aviala <aviala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 13:53:40 by aviala            #+#    #+#             */
-/*   Updated: 2013/11/22 13:53:41 by aviala           ###   ########.fr       */
+/*   Updated: 2013/11/27 10:23:47 by aviala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <string.h>
 #include "libft.h"
@@ -19,11 +18,14 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char		*pdst;
 	const unsigned char	*psrc;
 	size_t				i;
+	unsigned char		*tmp;
 
 	if (dest == 0 || src == 0)
 		return (0);
 	pdst = dest;
 	psrc = src;
+	tmp = ft_memalloc(n);
+
 	if (dest != src && n != 0)
 	{
 		i = 0;
