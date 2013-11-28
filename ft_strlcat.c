c9cc7ff43a638c9044ec77d13fe27ea355e30aef
Axel Viala <aviala@student.42.fr>
2013-11-28 19:12:01 +0100

    FUCKCKINNNGGG behaviour of strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: aviala <aviala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 19:09:13 by aviala            #+#    #+#             */
-/*   Updated: 2013/11/28 17:20:37 by aviala           ###   ########.fr       */
+/*   Updated: 2013/11/28 19:07:42 by aviala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,16 @@ size_t ft_strlcat(char *dst, const char *src, size_t size)
 	psrc = src;
 	dst_len = ((char *)ft_memchr(dst, '\0', size) - dst);
 	n = size - dst_len;
+	printf("n: %lu \n", n);
 	if (n == 0)
 		return (dst_len + ft_strlen(psrc));
 	while (*psrc != '\0')
 	{
-		if (n != 1)
+		if (n != 1 || size != 1)
 		{
 			*dst++ = *psrc;
 			n--;
+			size--;
 		}
 		psrc++;
 	}
