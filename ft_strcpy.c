d5d00ea6f380904cc0f1aaa8c57c584fb365eded
Axel Viala <aviala@student.42.fr>
2013-11-28 18:59:20 +0100

    patch ft_strcpy for mimic strcpy behaviour

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: aviala <aviala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/20 17:49:09 by aviala            #+#    #+#             */
-/*   Updated: 2013/11/22 13:55:19 by aviala           ###   ########.fr       */
+/*   Updated: 2013/11/28 18:56:54 by aviala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 
 char *ft_strcpy(char *s1, const char *s2)
 {
-	return ((s1 == 0 || s2 == 0) ? 0 : ft_strncpy(s1, s2, ft_strlen(s2)));
+	return (ft_strncpy(s1, s2, ft_strlen(s2) + 1));
 }
