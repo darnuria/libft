d9f6814cb352844c4b30766929b0527b2b64a660
Axel Viala <aviala@student.42.fr>
2013-11-28 16:57:29 +0100

    ft_strlcar.c ft_strncpy.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,35 +6,34 @@
 /*   By: aviala <aviala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 19:09:13 by aviala            #+#    #+#             */
-/*   Updated: 2013/11/23 19:04:02 by aviala           ###   ########.fr       */
+/*   Updated: 2013/11/28 16:56:07 by aviala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include "libft.h"
+#include <stdio.h>
 
 size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	len_src;
-	size_t	len_dst;
-	size_t	result;
+	const char	*psrc;
+	size_t		n;
+	size_t		dst_len;
 
-	len_src = ft_strlen(src);
-	len_dst = ft_strlen(dst);
-	if (size < len_dst)
+	psrc = src;
+	dst_len = (dst - (char *)ft_memchr(dst, '\0', size));
+	n = size - dst_len;
+	if (n == 0)
+		return (dst_len + ft_strlen(psrc));
+	while (*psrc != '\0')
 	{
-		while (*dst++ != '\0')
+		if (n != 1)
 		{
+			*dst++ = *psrc;
+			n--;
 		}
-		;
-		while ((*dst++ = *src++) && 0 < (int) (size - ft_strlen(src) - 1))
-			;
-		result = size + len_src;
+		psrc++;
 	}
-	else
-	{
-
-		result = len_src + len_dst;
-	}
-	return (result);
+	*dst = '\0';
+	return (dst_len + (psrc - src));
 }
