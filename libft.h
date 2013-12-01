0434465e292b2dde49f58025116d55eacbe0113f
Axel Viala <aviala@student.42.fr>
2013-12-01 23:29:42 +0100

    ft_lstadd.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aviala <aviala@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 16:07:32 by aviala            #+#    #+#             */
-/*   Updated: 2013/12/01 22:51:23 by aviala           ###   ########.fr       */
+/*   Updated: 2013/12/01 23:27:07 by aviala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ typedef struct	s_list
 	struct s_list	*next;
 }				t_list;
 
+void	ft_lstadd(t_list **alst, t_list *new);
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 t_list*	ft_lstnew(void const *content, size_t content_size);
 int		ft_atoi(const char *nptr);
