fa4ac89c68d2cfa87a8dc75044d4a01d0ea96b69
Axel Viala <axel.viala@gmail.com>
2013-12-12 16:02:03 +0100

    add standards define like EXIT_SUCCESS

---
+++ includes/ft_std.h
@@ -0,0 +1,17 @@
+#ifndef H_FT_STD
+# define H_FT_STD
+/*
+** Successful exit status.
+*/
+# ifndef EXIT_SUCCESS
+#  define EXIT_SUCCESS 0
+# endif /* !EXIT_SUCCESS */
+
+/*
+** Failling exit status
+*/
+# ifndef EXIT_FAILURE
+#  define EXIT_FAILURE 1
+# endif /* !EXIT_FAILURE
+
+#endif /* !H_FT_STD */
